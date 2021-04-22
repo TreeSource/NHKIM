b896a3d0b5c5db51ab711e7d57c742947e696c95
TreeSource <mskknh@gmail.com>
2021-04-21 20:50:03 -0400

    P364-366 justify.c

---
+++ C/C_Programming/Ch15_Writing_Large_Programs/word.h
@@ -0,0 +1,12 @@
+#ifndef WORD_H
+#define WORD_H
+
+/*
+
+read_word: Reads the next word from the input and store it
+in word. Makes word empty if no word could be read because
+of end-of-file. Truncate the word if its length exceeds len.
+
+*/
+void read_word(char *word, int len);
+#endif
